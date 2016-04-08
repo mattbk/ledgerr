5d463cd0683b2c5f470e2249b79ca1cf3a21a0c3
Matt <bedrocks@gmail.com>
2016-04-08 08:42:12 -0500

    initialize

---
+++ config.template.R
@@ -0,0 +1,3 @@
+# Copy each of the sheets by URL (from address bar)
+journal1<-gsheet2tbl("https://docs.google.com/spreadsheets/morestuffhere1")
+journal2<-gsheet2tbl("https://docs.google.com/spreadsheets/morestuffhere2")
