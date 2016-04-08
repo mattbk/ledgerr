ff85c27c29b0f2b9b6e7ca500b026852b10b7a41
Matt <bedrocks@gmail.com>
2016-04-08 11:00:51 -0500

    Add TODOs, start rough net functions

--- config.template.R
+++ config.template.R
@@ -1,3 +1,4 @@
 # Copy each of the sheets by URL (from address bar)
+# TODO return a list, not series of named variables
 journal1<-gsheet2tbl("https://docs.google.com/spreadsheets/morestuffhere1")
 journal2<-gsheet2tbl("https://docs.google.com/spreadsheets/morestuffhere2")
