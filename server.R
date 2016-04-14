881c635ad7f0f36b2ccd859c09786835d2d7f96e
Matt <bedrocks@gmail.com>
2016-04-14 17:08:05 -0500

    Get some Shiny stuff going.

--- server.R
+++ server.R
@@ -2,12 +2,40 @@
 # This is the server logic of a Shiny web application. You can run the 
 # application by clicking 'Run App' above.
 #
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+
+# Use https://github.com/maxconway/gsheet
+library(gsheet)
+
+# Use the functions in ledgerr (the package that is part of this project) but don't load the package itself--this will let us deploy to Shiny.io more easily.
+library(R.utils) #for sourceDirectory()
+sourceDirectory("ledgerr/R")
+
+# Pull Google sheet locations from another file (journal1, journal2, etc.)
+source("config.R")
+
+# Combine bank accounts
+journal<-rbind(journal1,journal2)
+
+## Clean up
+# Date is date
+journal$Date<-as.Date(journal$Date,format="%m/%d/%Y")
+# Replace empty classes and accounrs
+journal$Class[journal$Class==""]<-c("unclassified")
+journal$Account[journal$Account==""]<-c("unclassified")
+# Class, Accounts are factors
+journal$Class<-as.factor(journal$Class)
+journal$Account<-as.factor(journal$Account)
+# Empty class should be NA
+# Debit and Credit are numeric, without commas
+journal$Debit<-as.numeric(sub(",","",journal$Debit))
+journal$Credit<-as.numeric(sub(",","",journal$Credit))
+
+
+
+
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -21,6 +49,11 @@ shinyServer(function(input, output) {
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
     
+  })
+  
+  # Show journal
+  output$mytable = renderDataTable({
+    journal
   })
   
 })
